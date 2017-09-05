02d67743586893110d74046adfa568cef81060b1
mo <daamane1@abv.bg>
2017-09-05 08:25:01 +0300

    3d

---
+++ loopsInterview/minNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var min = int.MaxValue;
+            for (var i = 1; i <= n; i++)
+            {
+                var num = int.Parse(Console.ReadLine());
+                if (num < min)
+                    min = num;
+            }
+            Console.WriteLine("min = " + min);
+            Console.ReadKey();
+        }
+    }
+}
