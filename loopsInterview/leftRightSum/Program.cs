ef4208dc31bb4b353943487d89ac4c047acbddb2
mo <daamane1@abv.bg>
2017-09-05 08:16:24 +0300

    1st

---
+++ loopsInterview/leftRightSum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leftRightSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var leftSum = 0;
+            for (var i = 0; i < n; i++)
+                leftSum = leftSum + int.Parse(Console.ReadLine());
+
+            var rightSum = 0;
+            for (var i = n; i < 2*n; i++)
+                rightSum = rightSum + int.Parse(Console.ReadLine());
+
+            if (leftSum == rightSum)
+                Console.WriteLine("Yes, sum = " + leftSum);
+            else
+                Console.WriteLine("No, diff = " +
+                  Math.Abs(rightSum - leftSum));
+
+            Console.ReadKey();
+        }
+    }
+}
