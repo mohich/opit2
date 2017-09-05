4be6a66832c3f2c192f873b2dec639b44901be60
mo <daamane1@abv.bg>
2017-09-05 08:17:54 +0300

    2st

---
+++ loopsInterview/maxNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var max = int.MinValue;
+            for (var i = 1; i <= n; i++)
+            {
+                var num = int.Parse(Console.ReadLine());
+                if (num > max)
+                    max = num;
+            }
+            Console.WriteLine("max = " + max);
+            Console.ReadKey();
+        }
+    }
+}
